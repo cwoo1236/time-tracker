5fe821e9c2d1c0b4151c5f99edff900d0319d661
Christian Woo <cwoo1236@terpmail.umd.edu>
2023-10-12 02:35:15 -0400

    building server and routes

--- server/server.js
+++ server/server.js
@@ -1,32 +1,32 @@
 // get port # from config.env
 require("dotenv").config();
 
-// create express process?
+// create express process
+const mongoose = require('mongoose');
 const express = require("express");
-const app = express();
-// const cors = require("cors");
+const routes = require('./routes/record');
 
-// app.use(cors());
-// app.use(express.json());
-// app.use(require("./routes/record"));
-// // get driver connection
-// const dbo = require("./db/conn");
-// app.listen(port, () => {
-//   // perform a database connection when server starts
-//   dbo.connectToServer(function (err) {
-//     if (err) console.error(err);
-//    });
-//   console.log(`Server is running on port: ${port}`);
-// });
+const app = express();
+
+// middleware
+app.use(express.json());
+
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
-app.get('/', (req, res) => {
-  res.json({msg: "welcoem to the app"});
-});
+// routes
+app.use('/api/activities', routes);
 
-app.listen(process.env.PORT, () => {
-  console.log(`listening on port ${process.env.PORT}`);
-});
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+        console.log('Connected to db & listening on port', process.env.PORT);
+    });
+  })
+  .catch((error => {
+    console.log(error);
+  }));
